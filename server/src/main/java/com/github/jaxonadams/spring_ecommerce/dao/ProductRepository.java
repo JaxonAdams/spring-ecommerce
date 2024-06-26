a36592ab36446bf00a08a18c8750dc0f3620fc82
Jaxon Adams <jadamsresume1@gmail.com>
2024-06-26 09:44:36 -0600

    updated folder structure for client-side code

--- server/src/main/java/com/github/jaxonadams/spring_ecommerce/dao/ProductRepository.java
+++ server/src/main/java/com/github/jaxonadams/spring_ecommerce/dao/ProductRepository.java
@@ -2,5 +2,7 @@ package com.github.jaxonadams.spring_ecommerce.dao;
 
 import com.github.jaxonadams.spring_ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {}

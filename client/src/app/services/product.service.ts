932cbbdfbdf55b78132bcdb22205f98663f93c35
Jaxon Adams <jadamsresume1@gmail.com>
2024-06-26 14:33:07 -0600

    moved away from hardcoded categories

--- client/src/app/services/product.service.ts
+++ client/src/app/services/product.service.ts
@@ -4,28 +4,44 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products';
+  private baseUrl = 'http://localhost:8080/api';
 
   constructor(private httpClient: HttpClient) {}
 
   getProductList(categoryId: number): Observable<Product[]> {
 
-    const searchUrl: string = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
+    const searchUrl: string = `${this.baseUrl}/products/search/findByCategoryId?id=${categoryId}`;
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+
+    const categoryUrl: string = `${this.baseUrl}/product-category`;
+
+    return this.httpClient.get<GetResponseProductCategory>(categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[]
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[]
   }
 }
--- server/src/main/java/com/github/jaxonadams/spring_ecommerce/config/DataRestConfig.java
+++ server/src/main/java/com/github/jaxonadams/spring_ecommerce/config/DataRestConfig.java
@@ -2,14 +2,28 @@ package com.github.jaxonadams.spring_ecommerce.config;
 
 import com.github.jaxonadams.spring_ecommerce.entity.Product;
 import com.github.jaxonadams.spring_ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public void MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry corsRegistry) {
@@ -28,6 +42,29 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods))
                 .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods));
 
+        // call an internal helper method to expose ids
+        exposeIds(config);
+
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // expose entity ids
+
+        // get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entityTypes = entityManager.getMetamodel().getEntities();
+
+        // create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // get the entity types for the entities
+        for (EntityType entityType : entityTypes) {
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        // expose the entity ids for the array of entity types
+        Class[] domainTypes = entityClasses.toArray(new Class[entityClasses.size()]);
+        config.exposeIdsFor(domainTypes);
     }
 
 }

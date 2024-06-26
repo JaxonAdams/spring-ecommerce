26814443062d5afaa0881f6585ab281b87f02f6f
Jaxon Adams <jadamsresume1@gmail.com>
2024-06-25 21:22:50 -0600

    Added config to rest controllers to only allow GET methods

---
+++ server/src/main/java/com/github/jaxonadams/spring_ecommerce/config/DataRestConfig.java
@@ -0,0 +1,33 @@
+package com.github.jaxonadams.spring_ecommerce.config;
+
+import com.github.jaxonadams.spring_ecommerce.entity.Product;
+import com.github.jaxonadams.spring_ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry corsRegistry) {
+
+        HttpMethod[] unsupportedMethods = {HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE};
+
+        // disable HTTP methods for Product: PUT, POST, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods));
+
+        // disable HTTP methods for ProductCategory: PUT, POST, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedMethods));
+
+    }
+
+}
